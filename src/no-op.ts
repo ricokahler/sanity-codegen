52049e64d80aed835a9a836b1db100a458c48cca
Rico Kahler <rakahler@umich.edu>
2020-12-02 20:29:49 -0500

    fix: improve no-op with expando proxy (#14)

--- src/no-op.ts
+++ src/no-op.ts
@@ -1,2 +1,12 @@
-// Used to resolve the sanity part system to a no-op
-export default () => {};
+// Used to resolve the sanity part system to a no-op.
+// This ES6 proxy intercepts all properties calls and returns itself
+const proxy: any = new Proxy(
+  function () {
+    return proxy;
+  },
+  {
+    get: () => proxy,
+  }
+);
+
+module.exports = proxy;
