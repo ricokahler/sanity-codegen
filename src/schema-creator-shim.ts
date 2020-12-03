4774a7874302ed7eae9fe9dbcc5312f0a3ebf057
Rico Kahler <ricokahler@me.com>
2020-12-02 20:21:44 -0500

    fix: improve no-op with expando proxy

--- src/schema-creator-shim.ts
+++ src/schema-creator-shim.ts
@@ -1,6 +1,6 @@
 // The purpose of this file is to shim out the `createSchema` call from
 // `import createSchema from 'part:@sanity/base/schema-creator';`
-// so that it simple re-exports the uncompiled type
+// so that it simply re-exports the uncompiled type
 function createSchemaShim({ types }: any) {
   return types;
 }
