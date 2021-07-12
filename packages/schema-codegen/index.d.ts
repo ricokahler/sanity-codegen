88f6979104eafe49876e82ff6260d44faeb07e91
Rico Kahler <ricokahler@me.com>
2021-07-11 20:58:44 -0400

    refactor(schema-codegen): rename ambient to schema-codegen

--- packages/schema-codegen/index.d.ts
+++ packages/schema-codegen/index.d.ts
@@ -1,6 +1,6 @@
 // "imports" the ambient types so that the package root can be used as a
 // triple slash reference: https://stackoverflow.com/a/66951492/5776910
-/// <reference types="./ambient" />
+/// <reference types="./schema-codegen" />
 
 // re-exports the types put in `dist`
 export * from './dist';
