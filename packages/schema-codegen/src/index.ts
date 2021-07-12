ba10533d7b4c46c57e57f38c8c2c6f7de4747d3c
Rico Kahler <ricokahler@me.com>
2021-07-12 03:47:51 -0400

    chore: remove rogue console.log

--- packages/schema-codegen/src/index.ts
+++ packages/schema-codegen/src/index.ts
@@ -2,6 +2,3 @@ export * from './schema-extractor';
 export * from './default-babel-options';
 export * from './generate-schema-types';
 export * from './schema-normalizer';
-
-const x: Sanity.SchemaDef.FileNode = null as any;
-console.log(x);
