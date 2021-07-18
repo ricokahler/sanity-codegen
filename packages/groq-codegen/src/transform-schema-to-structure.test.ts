9fdd5689f1183225811e1f8a396aadb7ad2da741
Rico Kahler <ricokahler@me.com>
2021-07-18 12:57:01 -0400

    feat(groq-codegen): support blocks from schema

--- packages/groq-codegen/src/transform-schema-to-structure.test.ts
+++ packages/groq-codegen/src/transform-schema-to-structure.test.ts
@@ -84,4 +84,128 @@ describe('transformSchemaToStructure', () => {
       },
     });
   });
+
+  it('works with Blocks', () => {
+    const schema = schemaNormalizer([
+      {
+        name: 'book',
+        type: 'document',
+        fields: [
+          { name: 'title', type: 'string' },
+          {
+            name: 'description',
+            type: 'array',
+            of: [
+              {
+                type: 'block',
+                of: [
+                  { type: 'footNote' },
+                  {
+                    type: 'object',
+                    name: 'inlineDefinition',
+                    fields: [{ name: 'num', type: 'number' }],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'footNote',
+        type: 'object',
+        fields: [{ name: 'note', type: 'string' }],
+      },
+    ]);
+
+    const structure = transformSchemaToStructure({ schema });
+
+    expect(structure).toMatchObject({
+      type: 'Array',
+      of: {
+        type: 'Or',
+        children: [
+          {
+            type: 'Object',
+            properties: [
+              { key: '_type', value: { type: 'String', value: 'book' } },
+              { key: '_id', value: { type: 'String' } },
+              { key: 'title', value: { type: 'String' } },
+              {
+                key: 'description',
+                value: {
+                  type: 'Array',
+                  of: {
+                    type: 'Or',
+                    children: [
+                      {
+                        type: 'Object',
+                        properties: [
+                          { key: '_key', value: { type: 'String' } },
+                          {
+                            key: '_type',
+                            value: { type: 'String', value: 'block' },
+                          },
+                          {
+                            key: 'children',
+                            value: {
+                              type: 'Array',
+                              of: {
+                                type: 'Or',
+                                children: [
+                                  {
+                                    type: 'Object',
+                                    properties: [
+                                      {
+                                        key: '_key',
+                                        value: { type: 'String' },
+                                      },
+                                      {
+                                        key: '_type',
+                                        value: {
+                                          type: 'String',
+                                          value: 'span',
+                                        },
+                                      },
+                                      {
+                                        key: 'marks',
+                                        value: {
+                                          type: 'Array',
+                                          of: { type: 'Unknown' },
+                                        },
+                                      },
+                                      {
+                                        key: 'text',
+                                        value: { type: 'String' },
+                                      },
+                                    ],
+                                  },
+                                  { type: 'Lazy' },
+                                  {
+                                    type: 'Object',
+                                    properties: [
+                                      { key: 'num', value: { type: 'Number' } },
+                                    ],
+                                  },
+                                ],
+                              },
+                            },
+                          },
+                          {
+                            key: 'markDefs',
+                            value: { type: 'Array', of: { type: 'Unknown' } },
+                          },
+                          { key: 'style', value: { type: 'String' } },
+                        ],
+                      },
+                    ],
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    });
+  });
 });
