ecec1b7b00c1e9a893d67ada51db2e8bdd28ca41
Rico Kahler <ricokahler@me.com>
2021-07-25 17:54:25 -0400

    fix(groq-codegen): fix bug with unwrap reference

--- packages/groq-codegen/src/utils/unwrap-references.ts
+++ packages/groq-codegen/src/utils/unwrap-references.ts
@@ -1,20 +1,9 @@
 import { createStructure } from './create-structure';
-import { isStructureNull } from './is-structure-null';
-import { isStructureOptional } from './is-structure-optional';
-import { unwrapArray } from './unwrap-array';
-import { wrapArray } from './wrap-array';
 
 function unwrapLazy(n: Sanity.GroqCodegen.StructureNode) {
   switch (n.type) {
     case 'Lazy': {
-      const got = n.get();
-      if (n.hash === got.hash) {
-        throw new Error(
-          'Found self-referencing node when trying to unwrap a reference. ' +
-            'This is most likely a bug in sanity codegen. Please open an issue.',
-        );
-      }
-      return got;
+      return n.get();
     }
     case 'And':
     case 'Or': {
@@ -41,12 +30,6 @@ export function unwrapReferences(
       return createStructure({
         ...node,
         children: node.children.map(unwrapReferences),
-      });
-    }
-    case 'Array': {
-      return wrapArray(unwrapReferences(unwrapArray(node.of)), {
-        canBeNull: isStructureNull(node),
-        canBeOptional: isStructureOptional(node),
       });
     }
     case 'Lazy': {
