0b24403170253c30169bfbd900d890554b25a22e
Rico Kahler <ricokahler@gmail.com>
2021-11-14 17:37:35 -0500

    feat: update repo setup; release with lerna

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,2 +1,4 @@
+/// <reference types="@sanity-codegen/types" />
+
 export { run } from '@oclif/command';
 export * from './types';
--- packages/groq-codegen/index.d.ts
+++
@@ -1,5 +0,0 @@
-// "imports" the ambient types so that the package root can be used as a
-// triple slash reference: https://stackoverflow.com/a/66951492/5776910
-/// <reference types="@sanity-codegen/groq-codegen/groq-codegen" />
-
-export * from './dist';
