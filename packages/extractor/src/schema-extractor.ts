050268a5bb95d84a51ed42c108136748cfc6f7df
Oscar Chic Farré <oscar@origen.studio>
2023-03-29 15:49:39 +0200

    fix: add listeners for other events to better handle errors

--- packages/extractor/src/schema-extractor.ts
+++ packages/extractor/src/schema-extractor.ts
@@ -26,13 +26,38 @@ export async function schemaExtractor(params: ExecutorOptions) {
 
   childProcess.send(JSON.stringify(normalizedOptions));
 
-  const message = await new Promise<string>((resolve) => {
-    childProcess.on('message', resolve);
-  });
+  const messageOrResult = await Promise.race([
+    new Promise<string>((resolve) => {
+      // this covers the handled cases by the executor process, either being
+      // successful or an error
+      childProcess.on('message', resolve);
+    }),
+    new Promise<ExecutorResult>((resolve) => {
+      // this covers the unhandled errors by the executor process, such
+      // as top level syntax errors or not found modules
+      childProcess.on('error', (error) => resolve({ status: 'error', error }));
+    }),
+    new Promise<ExecutorResult>((resolve) => {
+      // this is a fallback in case the executor process does not send a
+      // message or error, as should always be the last promise to resolve
+      childProcess.on('close', (exitCode) =>
+        resolve({
+          status: 'error',
+          error:
+            typeof exitCode === 'number'
+              ? `'schema-executor' process exited with status code ${exitCode}`
+              : `'schema-executor' process exited without status code`,
+        }),
+      );
+    }),
+  ]);
 
   childProcess.kill();
 
-  const executorResult: ExecutorResult = JSON.parse(message);
+  const executorResult: ExecutorResult =
+    typeof messageOrResult === 'string'
+      ? JSON.parse(messageOrResult)
+      : messageOrResult;
 
   if (executorResult.status === 'error') {
     // TODO: ensure this serializes correctly and errors bubble up correctly
